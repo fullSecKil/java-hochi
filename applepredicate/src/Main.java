006c5f0cfb25a181ff89de17d159ff9d8fbebb97
Dusk <33350026+fullSecKil@users.noreply.github.com>
2018-12-22 23:51:57 +0800

    List类型抽象化
    lambda, 加泛型 list抽象化

--- applepredicate/src/Main.java
+++ applepredicate/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -12,7 +13,7 @@ import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) {
 
-        List<Apple> appleList = Arrays.asList(new Apple("green", 150), new Apple("red", 180));
+        List<Apple> appleList = Arrays.asList(new Apple("green", 170), new Apple("green", 150), new Apple("red", 180));
         Main m = new Main();
         /**
          * 方法引用谓词过滤
@@ -21,8 +22,39 @@ public class Main {
         // m.filterApples(appleList, Apple::isHeavyApple);
 
         m.filterApples(appleList, new AppleGreenColorPredicate());
+
+        // lambda 表达式
         List<Apple>  redApples = m.filterApples(appleList, a -> a.getWeight() > 150 && "red".equals(a.getColor()));
         System.out.println(redApples);
+
+        // 匿名类
+        List<Apple> redApples2 = m.filterApples(appleList, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        });
+        System.out.println(redApples2);
+
+        // 更加灵活，简洁
+        List<Apple> redApples3 = m.filter(appleList, a->a.getWeight()<151);
+        System.out.println(redApples3);
+
+        List<Integer> eventNumbers = m.filter(Arrays.asList(1, 2, 3, 4), i-> i%2 == 0);
+        System.out.println(eventNumbers);
+
+        // comparator(比较器) 排序
+        // 在Java 8中，List自带了一个sort(分类)方法。sort的行为可以用java.util.Comparator对象来参数化
+
+        // 匿名类
+        appleList.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        });
+        System.out.println(appleList);
+
     }
 
     /**
@@ -49,6 +81,18 @@ public class Main {
         for (Apple apple: inventory){
             if(p.test(apple)){
                 result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    // 将List类型抽象化, 泛型T使用任何类型对象
+
+    <T> List<T> filter(List<T> list, Predicate<T> p){
+        List<T> result = new ArrayList<>();
+        for (T e: list){
+            if (p.test(e)){
+                result.add(e);
             }
         }
         return result;
