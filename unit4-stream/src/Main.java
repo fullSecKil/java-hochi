e26e6db3898cadb5c6d66b5fc71db2988bc0f279
Dusk <33350026+fullSecKil@users.noreply.github.com>
2019-01-08 23:44:53 +0800

    流的内部迭代，中间操作链形成一条流水线
    第四章的结束 流利用内部迭代：迭代通过filter、map、sorted等操作被抽象掉了。流链接在一起用它们来设置一条流水线，但并不会生成任何结果。

--- unit4-stream/src/Main.java
+++ unit4-stream/src/Main.java
@@ -89,6 +89,65 @@ public class Main {
         /**
          *  集合与流之间的差异就在于什么时候进行计算(dvd与在线视频例子)
          */
+        // 和迭代器类似，流只能遍历一次。遍历完之后，我们就说这个流已经被消费掉了
+        // 无法执行两次终端操作
+
+        // 使用Collection接口需要用户去做迭代（比如用for-each），这称为外部迭代，相反stream库用内部迭代----他帮你把迭代做了，还把得到的流值存在了某个地方，你只要给出一个函数要干什么就可以了
+
+        List<String> names = new ArrayList<>();
+        // 集合：用for-each循环外部迭代
+        for(Dish d: menu){
+            names.add(d.getName());
+        }
+        // for-each 隐藏了迭代中一些复杂性。for-each结构是一个语法糖，它背后的东西用Iterator对象表达出来更要丑陋得多。
+
+        List<String> names2 = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()){
+            Dish d = iterator.next();
+            names2.add(d.getName());
+        }
+        // 集合：用背后的迭代器做外部迭代
+
+        // 流：内部迭代
+        List<String> names3 = menu.stream().map(Dish::getName).collect(Collectors.toList());        // 开始操作流水线，没有迭代
+
+        /**
+         *  java.util.stream stream接口定义了许多操作分为中间操作 和 终端操作
+         */
+
+        List<String> names4 = menu.stream()                                                 // 从菜单获得流
+                                                        .filter(d->{
+                                                            System.out.println("filtering" + d.getName());
+                                                            return d.getCalories() > 300;
+                                                        })           // 中间操作
+                                                        .map(d->{
+                                                            System.out.println("mapping" + d.getName());
+                                                            return d.getName();
+                                                        })                            // 中间操作
+                                                        .limit(3)                                                  // 中间操作
+                                                        .collect(Collectors.toList());                  // 将stream转为list
+
+        // filter、map和limit可以连成一条流水线  collect触发流水线执行并关闭它。
+
+        // 可以连接起来的流操作称为中间操作，关闭流的操作称为终端操作。
+
+        // 打印可以看到是同时，尽管filter和map是两个独立的操作，但它们合并到同一次遍历中了（我们把这种技术叫作循环合并）
+
+        /**
+         *  终端操作
+         */
+
+        menu.stream().forEach(System.out::println);     // forEach是一个返回void的终端操作，它会对源中的每道菜应用一个Lambda
+
+        // 终端操作会从留的流水线生成结果。 其结果是任何不适流的值比如List、Integer，void
+
+        /**
+         *  流的使用一般包括三件事
+         *   一个数据源（如集合）来执行一个查询
+         *   一个中间操作链，形成一条流的流水线
+         *   一个终端操作，执行流水线，并能生成结果
+         */
     }
 
 
